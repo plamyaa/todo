96a058fc4dc123f6f87d43b96750ac0c281bcd5c
bielberg <101133918+bielberg@users.noreply.github.com>
2022-03-07 21:32:38 +0300

    Add files via upload

---
+++ view.js
@@ -0,0 +1,4 @@
+export const BtnsAdd = document.querySelectorAll('.button__plus');
+export const highFormValue = document.querySelector('.input__text');
+export const lowFormValue = document.querySelector('.inputlow__text');
+export const isEmpty = '';
