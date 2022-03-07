c289340a1c8a484d71c3417aca0f5a5b87529169
bielberg <101133918+bielberg@users.noreply.github.com>
2022-03-07 21:31:47 +0300

    Delete css,js directory

--- css,js/view.js
+++
@@ -1,4 +0,0 @@
-export const BtnsAdd = document.querySelectorAll('.button__plus');
-export const highFormValue = document.querySelector('.input__text');
-export const lowFormValue = document.querySelector('.inputlow__text');
-export const isEmpty = '';
